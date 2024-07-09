9b4718bc0229e2afa085880c8ad07261dd38547a
KARPAGAKIRTHIKA <103020162+KARPAGAKIRTHIKA@users.noreply.github.com>
2024-07-09 14:55:25 +0530

    Update HeaderComponent.jsx

--- src/assets/Components/HeaderComponent.jsx
+++ src/assets/Components/HeaderComponent.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-function Headercomponent(props) {
+function HeaderComponent(props) {
     const [isClicked, setIsClicked] = useState(false);
 
     const handleClick = () => {
@@ -24,4 +24,4 @@ function Headercomponent(props) {
   )
 }
 
-export default Headercomponent;
+export default HeaderComponent;
