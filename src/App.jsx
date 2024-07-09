ee29800b6f1b76648c620c470347a1d6a1830121
KARPAGAKIRTHIKA <103020162+KARPAGAKIRTHIKA@users.noreply.github.com>
2024-07-09 14:56:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import { FaRegEye } from "react-icons/fa6";
-import Headercomponent from './assets/Components/HeaderComponent';
+import HeaderComponent from './assets/Components/HeaderComponent';
 function App() {
   
 
   return (
     <>
     <div className='box'><FaRegEye  className='icon'/>
-    <Headercomponent name="KARPAGAKIRTHIKA" regno="212221220025"/>
+    <HeaderComponent name="KARPAGAKIRTHIKA" regno="212221220025"/>
     </div>
    
 
