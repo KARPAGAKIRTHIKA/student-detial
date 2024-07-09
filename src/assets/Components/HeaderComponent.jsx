af7813de93b2ecb0e31e7f834fcc40f4eadc68b8
karpagakirthika <velusamyg2004@gmail.com>
2024-07-09 14:16:51 +0530

    first commit

---
+++ src/assets/Components/HeaderComponent.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+function Headercomponent(props) {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+      setIsClicked(!isClicked);
+    };
+  return (
+    
+    <div><h3>NAME: {props.name}</h3>
+    <p className="Regno"> RegisterNo: {props.regno}</p>
+    <button onClick={handleClick} className="toggle-button">
+        {isClicked ? 'Hide Details' : 'Show Details'}
+      </button>
+      {isClicked && (
+        <p className="message">
+         Additional details or content can go here...
+        </p>
+      )}
+    </div>
+   
+    
+  )
+}
+
+export default Headercomponent;
